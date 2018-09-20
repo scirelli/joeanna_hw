d2d40e935e5e6e589f22a34465af676a56734588
kls602 <stephen.cirelli@capitalone.com>
2018-09-20 00:30:59 -0400

    First commit

---
+++ main.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define WELCOME "\t\tWelcome to my Program!\n\n"
+#define MENU "Menu Options:\n"\
+             "\tc) Change square's border character. (currently: '%c')\n"\
+             "\tn) Change square's width. (currently: '%d')\n"\
+             "\t1) Print a square type 1.\n"\
+             "\t2) Print a square type 2.\n"\
+             "\tq) Quit.\n"\
+             "--> "
+#define BAD_MENU_OPTION "\n\nNOT A VALID OPTION\n\n"
+#define SALUTATION "Good bye.\n"
+#define CHANGE_BOARDER_MESSAGE "Enter a new border character: "\
+                               "--> "
+
+const char INVALID                  = '\0';
+const char MENU_OPTION_BORDER_CHAR  = 'c';
+const char MENU_OPTION_WIDTH        = 'n';
+const char MENU_OPTION_PRINT_TYPE_1 = '1';
+const char MENU_OPTION_PRINT_TYPE_2 = '2';
+const char MENU_OPTION_QUIT         = 'q';
+
+const char DEFAULT_BORDER_CHAR = '*';
+const unsigned int DEFAULT_SQUARE_WIDTH = 6;
+
+char borderCharacter = DEFAULT_BORDER_CHAR;
+unsigned int width = DEFAULT_SQUARE_WIDTH;
+
+bool isValidMenuOption(char c)
+{
+    switch(c)
+    {
+        case MENU_OPTION_BORDER_CHAR:
+        case MENU_OPTION_WIDTH:
+        case MENU_OPTION_PRINT_TYPE_1:
+        case MENU_OPTION_PRINT_TYPE_2:
+        case MENU_OPTION_QUIT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+char printMenu()
+{
+    char line[4];
+    char response = INVALID;
+
+    printf(MENU, borderCharacter, width);
+    if(fgets(line, sizeof line, stdin) != NULL)
+    {
+        line[0] = tolower(line[0]);
+        return line[0];
+    }
+
+    return response;
+}
+
+void changeBorderCharacter()
+{
+    char line[4];
+    printf("\nPlease enter a new border character --> ");
+    if(fgets(line, sizeof line, stdin) != NULL)
+    {
+        borderCharacter = line[0];
+    }else
+    {
+        printf("Sorry, I was not able to read your character.");
+    }
+}
+
+void changeBorderWidth()
+{
+    int newWidth = 0;
+
+    printf("\nPlease enter a width --> ");
+    int result = scanf("%d", &newWidth);
+
+    getchar(); //Remove remaining newline
+
+    if(result == EOF)
+    {
+        printf("Sorry, I was not able to read your character.");
+    } else {
+        width = (unsigned int)newWidth;
+    }
+        printf("\n\n");
+}
+
+void printRectangle(char borderCharacter, char fillCharacter, unsigned int width, unsigned int height)
+{
+    if(width == 0 || height == 0)
+    {
+        printf("Width and height must be greater than zero.");
+        return;
+    }
+
+    if(width == 1 || height == 1)
+    {
+        printf("%c\n\n", borderCharacter);
+        return;
+    }
+
+    for(int x=0; x<width; x++){
+        printf("%c", borderCharacter);
+    }
+    printf("\n");
+    for(int y=0; y<(int)height-2; y++){
+        printf("%c", borderCharacter);
+        for(int x=0; x<(int)width-2; x++){
+            printf("%c", fillCharacter);
+        }
+        printf("%c\n", borderCharacter);
+    }
+    for(int x=0; x<width; x++){
+        printf("%c", borderCharacter);
+    }
+    printf("\n\n");
+}
+
+void printSquare(char borderCharacter, char fillCharacter, unsigned int width)
+{
+    printRectangle(borderCharacter, fillCharacter, width, width);
+}
+
+void printSquareType1()
+{
+    printSquare(borderCharacter, borderCharacter, width);
+}
+
+void printSquareType2()
+{
+    printSquare(borderCharacter, ' ', width);
+}
+
+int main(int argc, char* argv[])
+{
+    char response = INVALID;
+    
+    printf(WELCOME);
+    while( (response = printMenu()) != MENU_OPTION_QUIT )
+    {
+        printf("%c\n\n", response);
+        if(isValidMenuOption(response)){
+            switch(response){
+                case MENU_OPTION_BORDER_CHAR:
+                    changeBorderCharacter();
+                    break;
+                case MENU_OPTION_WIDTH:
+                    changeBorderWidth();
+                    break;
+                case MENU_OPTION_PRINT_TYPE_1:
+                    printSquareType1();
+                    break;
+                case MENU_OPTION_PRINT_TYPE_2:
+                    printSquareType2();
+                    break;
+                default:
+                    printf("This wasn't supposed to happen!!"); 
+                    return EXIT_FAILURE;
+            }
+        }else{
+            printf(BAD_MENU_OPTION);
+        }
+    }
+
+    printf(SALUTATION);
+    return EXIT_SUCCESS;
+}
